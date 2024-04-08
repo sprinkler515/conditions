0e3162a6f0ce4a972074a3f856bec88110a484b0
Tommy Chanthavisouk <wildcodeschool.sprinkler515@pm.me>
2024-04-08 14:22:42 +0200

    create main program

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿namespace playground
+{
+    using System;
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int limitOne, limitTwo, n, min, max;
+
+            Console.Write("Limit1: ");
+            limitOne = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Limit2: ");
+            limitTwo = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Integer: ");
+            n = Convert.ToInt32(Console.ReadLine());
+
+            min = limitOne < limitTwo ? limitOne : limitTwo;
+            max = limitOne > limitTwo ? limitOne : limitTwo;
+
+            if (n < min)
+            {
+                Console.WriteLine("You have entered {0} which is lower than {1} which is the minimum", n, min);
+                Console.WriteLine("Resulting integer: {0}", min);
+
+            }
+            else if (n > max)
+            {
+                Console.WriteLine("You have entered {0} which is greater than {1} which is the maximum", n, max);
+                Console.WriteLine("Resulting integer: {0}", max);
+            }
+        }
+    }
+}
